5e2f05d48d85344943f40eb07bde43d958ba707b
jmason87 <joewebdev87@gmail.com>
2022-01-11 17:37:39 -0600

    initial commit

---
+++ animals.py
@@ -0,0 +1,177 @@
+from datetime import date
+
+
+class Turtle:
+    """class for Turtle"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+tom = Turtle("Tom", "Snapping Turtle")
+print(tom.swimming)
+
+
+class Dolphin:
+    """class for Dolphin"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+dorris = Dolphin("Dorris", "Bottlenose Dolphin")
+print(dorris)
+
+
+class Shark:
+    """class for Shark"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+sam = Shark("Sam", "Great White")
+print(sam)
+
+class Crab:
+    """class for Crab"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+corey = Crab("Corey", "King Crab")
+print(corey)
+
+class Whale:
+    """class for Whale"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+wally = Whale("Wally", "Blue Whale")
+print(wally)
+
+class Monkey:
+    """class for Monkey"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+matt = Monkey("Matt", "Orangutan")
+print(matt)
+
+class Tiger:
+    """class for Tiger"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+tim = Tiger("Tim", "Siberian Tiger")
+print(tim)
+
+
+class Bear:
+    """class for Bear"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+benny = Bear("Benny", "Grizzly Bear")
+print(benny)
+
+
+class Lion:
+    """class for Lion"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+leo = Lion("Leo", "Mountian Lion")
+print(leo)
+
+
+class Zebra:
+    """class for Zebra"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+zach = Zebra("Zach", "Plains Zebra")
+print(zach)
+
+
+class Cobra:
+    """class for Cobra"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+connie = Cobra("Connie", "King Cobra")
+print(connie)
+
+
+class Python:
+    """class for Python"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+pete = Python("Pete", "Snake")
+print(pete)
+
+
+class Viper:
+    """class for Viper"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+vicky = Viper("Vicky", "Pit Viper")
+print(vicky)
+
+
+class Gardner:
+    """class for Gardner"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+gary = Gardner("Gary", "Gartner Snake")
+print(gary)
+
+
+class Anaconda:
+    """class for Anaconda"""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+andy = Anaconda("Andy", "Big Snake")
+print(andy)
